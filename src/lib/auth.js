449816034c4b16b17d0d81b1552c326348deb596
Imran Al Farabi <imranalfarabidevworks@gmail.com>
2026-05-21 15:42:59 +0600

    fix: mongodb family 4

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,10 +2,10 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 
-// const dns = require("node:dns");
-// dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient(process.env.MONGODB_URI, {
+  family: 4,
+  serverSelectionTimeoutMS: 10000,
+});
 
 export const auth = betterAuth({
   database: mongodbAdapter(client.db("docappoint")),
